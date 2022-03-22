0c3f3d29e6c8c1acc41b1c96147423ce48c57107
Akhilesh1902 <akhil1922002@gmail.com>
2022-03-22 19:05:28 +0530

    added tasty treats website

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,9 +1,10 @@
-import threeJsDemo from "../assets/thumbnails/3jsDemo.png";
-import flyingPan from "../assets/thumbnails/flyingPan.png";
-import pastry from "../assets/thumbnails/pastryShop.png";
-import rumbaChat from "../assets/thumbnails/rumba-chat.png";
-import weather from "../assets/thumbnails/weather-app.png";
+import threeJsDemo from "../assets/thumbnails/3jsDemo.png"
+import flyingPan from "../assets/thumbnails/flyingPan.png"
+import pastry from "../assets/thumbnails/pastryShop.png"
+import rumbaChat from "../assets/thumbnails/rumba-chat.png"
+import weather from "../assets/thumbnails/weather-app.png"
+import tastyTreats from "../assets/thumbnails/tastyTreats.png"
 
-const Images = [threeJsDemo, flyingPan, pastry, rumbaChat, weather];
+const Images = [tastyTreats, threeJsDemo, flyingPan, pastry, rumbaChat, weather]
 
-export default Images;
+export default Images
